edada01886043ddac5cfd90ef30353baf0c66545
Steve Schoonooghe <Steve.Schoonooghe@gmail.com>
2015-10-13 15:13:48 +0200

    Inherit table per class

--- fietsacademy/src/main/java/be/vdab/entities/Cursus.java
+++ fietsacademy/src/main/java/be/vdab/entities/Cursus.java
@@ -5,14 +5,12 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED) @Table(name = "cursussen")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) 
 public abstract class Cursus implements Serializable {
- 
 	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String naam; 
 	
